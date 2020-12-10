10cbdc139e9b6cbca49a6d87be79d21982c02c3d
Alex Ros <63934839+AlexRos10@users.noreply.github.com>
2020-12-10 18:29:56 +0100

    Day 10

---
+++ day10.py
@@ -0,0 +1,34 @@
+with open('input10.txt') as file:
+    numbers = file.readlines()
+
+adapters = []
+for i in numbers:
+    adapters.append(int(i))
+
+adapters.sort()
+jolt = 0
+one_diff = 0
+three_diff = 1
+for i in adapters:
+    if i - jolt == 1:
+        one_diff += 1
+    elif i - jolt == 3:
+        three_diff += 1
+    
+    jolt = i
+
+print(one_diff * three_diff)
+
+adapters.append(max(adapters) + 3)
+paths = [1, 1, 2]
+use_path = [False] * (adapters[-1] + 1)
+for i in adapters:
+    use_path[i] = True
+
+for i in range(3, len(use_path)):
+    p = paths[2] + paths[1] + paths[0]
+    paths[0] = paths[1]
+    paths[1] = paths[2]
+    paths[2] = p * use_path[i]
+
+print(paths[2])
